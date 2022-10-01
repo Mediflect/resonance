5596d9a2f562a4a69b9424a0edfda5e85a46386e
Jake Shields <rachiron.js@gmail.com>
2022-10-01 14:46:51 -0700

    torus light torus light

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -12,7 +12,6 @@ public static class Helpers
         {
             float tValue = Mathf.InverseLerp(0, transitionTime, timer);
             float tValueSmoothed = Mathf.SmoothStep(0, 1, tValue);
-            Debug.Log($"{tValue}, {tValueSmoothed}");
             obj.position = Vector3.Lerp(start.position, end.position, tValueSmoothed);
             yield return null;
             timer += Time.deltaTime;
