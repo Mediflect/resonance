ba8bb16ecd424daa94a4791daeeb782f37fc3bb5
Jake Shields <rachiron.js@gmail.com>
2022-10-01 18:20:33 -0700

    Respawn point work, bell sound file

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -1,8 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RespawnPoint : MonoBehaviour
 {
+    public enum ActiveState
+    {
+        Player,
+        Object,
+        Inactive
+    }
+
+    public ActiveState state = ActiveState.Inactive;
     public Transform spawnTransform;
+    public GameObject activePlayerLights;
+    public GameObject activeObjectLights;
+    public GameObject inactiveLights;
+
+    public void SetActiveState(ActiveState state)
+    {
+        this.state = state;
+        activePlayerLights.SetActive(state == ActiveState.Player);
+        activeObjectLights.SetActive(state == ActiveState.Object);
+        inactiveLights.SetActive(state == ActiveState.Inactive);
+    }
+
+    private void Awake()
+    {
+        SetActiveState(state);
+    }
+
+    [ContextMenu("RefreshState")]
+    private void DebugRefreshState()
+    {
+        SetActiveState(state);
+    }
 }
